e5ac1465e28c6c12ed50fc71dd04e55941efa8c1
Glasovski <glasfcsm520@mail.ru>
2023-08-26 21:30:52 +0300

    Определение веса по дз

---
+++ Hello world/src/Next.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Next {
+    public static void main(String[] args) {
+        System.out.println("\t\t\t***Программа определения весов товаров***\n\n\n");
+
+        System.out.println("Введите первый вес: ");
+        double first = new Scanner(System.in).nextDouble();
+
+        System.out.println("Введите второй вес: ");
+        double second = new Scanner(System.in).nextDouble();
+
+        System.out.println("Введите третий вес: ");
+        double third = new Scanner(System.in).nextDouble();
+
+        double greatest = 0;
+        double average = 0;
+        double smallest = 0;
+        //todo дописать логику программы ниже.
+        if(first > second && first > third){
+            greatest = first;
+        }else if(second > first && second > third) {
+            greatest = second;
+        } else if (third > second && third > first) {
+            greatest = third;
+        }
+
+        if(first < second && first < third){
+            smallest = first;
+        }else if(second < first && second < third) {
+            smallest = second;
+        } else if (third < second && third < first) {
+            smallest = third;
+        }
+
+        if((first > second && first < third) || (first < second && first > third)) {
+            average = first;
+        } else if ((second > first && second < third) ||(second < first && second > third)) {
+            average = second;
+        } else if ((third > first && third < second) || (third < first && third > second)) {
+            average = third;
+        }
+
+
+        //todo
+
+
+        System.out.println("Наибольший вес: " + greatest);
+        System.out.println("Средний вес: " + average);
+        System.out.println("Наименьший вес: " + smallest);
+    }
+}
